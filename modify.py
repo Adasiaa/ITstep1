9a35dbe5fd6cb97af4997109716e10b5c7dd22be
Adam12300 <adamrazin930@gmail.com>
2025-03-29 11:55:49 +0200

    КЛ робота

--- modify.py
+++ modify.py
@@ -54,6 +54,7 @@
 # dog1=Dog("Сінді")
 # print(dog1.name)
 
+
 # class Dog:
 #     def __init__(self, name):
 #         self.name = name
@@ -74,24 +75,83 @@
 # dog1=D("Сінді")
 # print(dog1.info())
 
-class Person:
-    def __init__(self, name, age, salary):
-        self.name = name
-        self.age = age
-        self.__salary = salary
-    def info(self):
-        print("Вітаю! Мене звати", self.name)
-        self._infoAge()
-        self.__infoSalary()
-    def _infoAge(self):
-            print('Мій вік', self.age)
-    def _infoSalary(self):
-        print('Моя ЗП', self.__salary)
-class Employee(Person):
-    def __init__(self, name, age, salary):
-        super().__init__(name, age, salary)
-        self.pos=pos
-    def printInfo(self):
-        print('Моя посада', self.pos)
-        print('Мій вік', self.age)
-        print('Моя ЗП', self.__salary)
+# class Person:
+#     def __init__(self, name, age, salary):
+#         self.name = name
+#         self.age = age
+#         self.__salary = salary
+#     def info(self):
+#         print("Вітаю! Мене звати", self.name)
+#         self._infoAge()
+#         self.__infoSalary()
+#     def _infoAge(self):
+#             print('Мій вік', self.age)
+#     def __infoSalary(self):
+#         print('Моя ЗП', self.__salary)
+# class Employee(Person):
+#     def __init__(self, name, age, salary, pos):
+#         super().__init__(name, age, salary)
+#         self.pos=pos
+#     def printInfo(self):
+#         print('Моя посада', self.pos)
+#         print('Мій вік', self.age)
+#         # print('Моя ЗП', self.__salary) помилка у достуі захищенності
+#
+# human=Person('Олеся', 20, 2000)
+# emp=Employee('Петро', 25, 45000, 'інженер')
+# print(human.name)
+# human.info()
+# print(emp.name)
+# emp.printInfo()
+# print(emp.age)
+# print(emp._Person__salary)
+
+
+import random as r
+
+
+class Character:
+    def __init__(self, name, health):
+        self.__name = name
+        self.__health = r.randint(0, 100)
+
+    def infoName(self):
+        return self.__name
+
+    def infoHP(self):
+        return self.__health
+
+    def attack(self):
+        pass
+
+    def take_damage(self):
+        self.__health -= r.randint(0, 10)
+
+    def is_alive(self):
+        return self.__health > 0
+
+    def __str__(self):
+        return self.__name + " моє здоров'я: " + str(self.__health)
+
+
+class Warrior(Character):
+    def __init__(self, name):
+        super().__init__(name, r.randint(80, 120))
+
+    def attack(self):
+        return ". Я атакую мечем"
+
+
+class Mage(Character):
+    def __init__(self, name):
+        super().__init__(name, r.randint(30, 80))
+
+    def attack(self):
+        return ". Я атакую магією"
+
+
+warrior = Warrior("Мене звати Артур")
+mage = Mage("Мене звати Генадій")
+
+print(str(warrior)+warrior.attack())
+print(str(mage)+mage.attack())
